1ae866cd7726d990b2549bb595b29f8074a817d7
Richard Harvey <richard@squarecows.com>
2012-03-21 16:48:15 +0000

    fixed path when files are extracted

--- scripts/post.d/00-unpack-sitecontent.sh
+++ scripts/post.d/00-unpack-sitecontent.sh
@@ -7,7 +7,7 @@ for i in SiteContent.tgz ; do
     echo "### Unpacking previous content ###"
         if [[ ${i} == "SiteContent.tgz" ]]; then STATIC_DIR=/var/www/public/ ; fi
         mkdir ${STATIC_DIR}
-        tar xvfz /var/tmp/c4-bootstrap/${i} -C ${STATIC_DIR}
+        tar xvfz /var/tmp/c4-bootstrap/${i} -C ${STATIC_DIR} --strip-components 3
     fi ;
 done
 
